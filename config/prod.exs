f3a1ed6f19f355a7f2c052a988ebecc5ee7df955
Eoic <karolis.strazdas@protonmail.com>
2022-11-17 22:48:00 +0200

    Update the main page.

--- config/prod.exs
+++ config/prod.exs
@@ -52,4 +52,5 @@ config :personal, PersonalWeb.Endpoint,
   http: [port: {:system, "PORT"}], # Possibly not needed, but doesn't hurt
   url: [host: System.get_env("APP_NAME") <> ".gigalixirapp.com", port: 443],
   secret_key_base: Map.fetch!(System.get_env(), "SECRET_KEY_BASE"),
+  force_ssl: [rewrite_on: [:x_forwarded_proto]],
   server: true
